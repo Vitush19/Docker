3933873ed6727b69920c7235a885783c356ed04e
Vitush19 <sutharsanvithusan@gmail.com>
2020-11-03 17:40:01 +0100

    3 images

--- scripts/build.sh
+++
@@ -1,2 +0,0 @@
-cd ./simple-api
-mvn clean verify org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.projectKey=Vitush19_Docker
--- scripts/docker.sh
+++
@@ -1,6 +0,0 @@
-# Connexion à Docker
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-# Build
-docker build -t $DOCKER_USERNAME/tp-devops .
-# Publication des images
-docker push $DOCKER_USERNAME/tp-devops
